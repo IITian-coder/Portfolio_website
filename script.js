eab6995ba91af95a42a5e68db43b5a741caa0ec4
IITian-coder <122205746+IITian-coder@users.noreply.github.com>
2023-03-22 22:35:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// Header toggle
+let MenuBtn = document.getElementById('MenuBtn')
+
+MenuBtn.addEventListener('click', function(e) {
+    document
+    .querySelector('body').classList.toggle('mobile-nav-active');
+    this.classList.toggle('fa-xmark');
+})
+
+
+// Typing effect
+
+let typed = new Typed('.auto-input', {
+    strings: ['Front-End Developer!', 'Freelancer!', "Student!"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 2000,
+    loop: true,
+})
+
+
+
